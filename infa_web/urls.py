0c21043786e7b53bb4c37105916aeabb5a1e4637
Julian <alka65@hotmail.com>
2016-08-24 09:19:14 -0500

    static heroku

--- infa_web/urls.py
+++ infa_web/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = [
 	url(r'^admin/', admin.site.urls),
@@ -8,4 +9,5 @@ urlpatterns = [
 	url('', include('infa_web.apps.movimientos.urls')),
 	url('', include('infa_web.apps.articulos.urls')),
 	url('', include('infa_web.apps.inventarios.urls')),
+	url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 ]
