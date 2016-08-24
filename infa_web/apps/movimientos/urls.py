eba58c90441a62f7e2277da30e3b93bfbc513895
Julian <alka65@hotmail.com>
2016-08-24 10:04:23 -0500

    up

--- infa_web/apps/movimientos/urls.py
+++ infa_web/apps/movimientos/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, include, url
 from infa_web.apps.movimientos.views import *
 
 urlInputMovement = [
+	url(r'^output-movement/add/$', OutputMovementCreate.as_view(), name = 'add-output-movements'),
+
 	url(r'^input-movement/$', InputMovementList.as_view(), name = 'list-input-movements'),
 	url(r'^input-movement/add/$', InputMovementCreate.as_view(), name = 'add-input-movement'),
 	url(r'^input-movement/edit/(?P<pk>\d+)/$', InputMovementUpdate, name = 'edit-input-movement'),
