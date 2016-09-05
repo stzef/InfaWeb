421a4da50d240aab36c038a7dd71b40cb06b76f1
carlosturnerbenites <carlosturnerbenites@gmail.com>
2016-09-05 15:25:46 -0500

    Avance en proceso de calculo de cantidades y costos

--- infa_web/routines.py
+++ infa_web/routines.py
@@ -3,6 +3,7 @@ from infa_web.parameters import ManageParameters
 from infa_web.apps.articulos.models import Arlo
 from infa_web.apps.inventarios.models import *
 from infa_web.apps.movimientos.models import *
+import operator
 
 manageParameters = ManageParameters()
 
@@ -18,8 +19,10 @@ def calcular_costo_articulo(carlos,nueva_cantidad,nuevo_costo,is_input):
 		type_costing_and_stock = manageParameters.get_param_value("type_costing_and_stock")
 		if(type_costing_and_stock == "M"):
 			return False
-
 		articulo = Arlo.objects.get(carlos=carlos)
+
+		print "-------------------------"
+		print "%s -> %s" % (articulo.canti,articulo.vcosto)
 
 		if is_input:
 			nuevo_costo_calculado = calculo_costo(articulo.canti,articulo.vcosto,nueva_cantidad,nuevo_costo,is_input)
@@ -30,6 +33,8 @@ def calcular_costo_articulo(carlos,nueva_cantidad,nuevo_costo,is_input):
 			articulo.canti -= nueva_cantidad
 
 		articulo.save()
+		print "%s -> %s" % (articulo.canti,articulo.vcosto)
+		print "-------------------------"
 
 		return True
 	else:
@@ -45,8 +50,6 @@ def calculo_cantidad_costo():
 
 
 	for articulo in articulos:
-		print separador
-		print articulo
 		if Invinideta.objects.filter(cii=invinicab,carlos=articulo).exists():
 			invinideta = Invinideta.objects.get(cii=invinicab,carlos=articulo)
 		else:
@@ -55,25 +58,15 @@ def calculo_cantidad_costo():
 		mvendeta = Mvendeta.objects.select_related().order_by('-cmven__fmven').filter(carlos=articulo)
 		mvsadeta = Mvsadeta.objects.select_related().order_by('-cmvsa__fmvsa').filter(carlos=articulo)
 
-		#print separador
-		#print mvendeta
-		#print separador
-		#print mvsadeta
-		print separador
 		mv = list(mvendeta) + list(mvsadeta)
-		print separador
-		print mv
-		print separador
-		print invinideta
-		##mv = Mven.objects.raw('SELECT movimientos_mven.*, movimientos_mvsa.* FROM movimientos_mven, movimientos_mvsa WHERE movimientos_mven.cmven.carlos_id = %s AND movimientos_mven.cmvsa.carlos_id = %s ORDER BY fmven,fmvsa', [articulo.pk,articulo.pk])
-		for x in mv:
-			print x
-			print type(x)
-			if hasattr(x, "cmeven"):
-				calcular_costo_articulo(articulo.carlos,x.canti,x.vunita,True)
-			else:
-				calcular_costo_articulo(articulo.carlos,x.canti,x.vunita,False)
-		print mv
+		if invinideta:
+			for x in mv:
+				if hasattr(x, "cmven"):
+					calcular_costo_articulo(invinideta.carlos.carlos,x.canti,x.vtotal,True)
+				else:
+					calcular_costo_articulo(invinideta.carlos.carlos,x.canti,x.vtotal,False)
+		else:
+			print "El articulo no se encuentra en el inventario"
 	return True
 
 
