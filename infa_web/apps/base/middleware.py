847bcb1b596f45ea01aab2d4e6da9367025a9bbe
carlosturnerbenites <carlosturnerbenites@gmail.com>
2016-09-08 11:19:56 -0500

    Modificaciones a movimientos

--- infa_web/apps/base/middleware.py
+++ infa_web/apps/base/middleware.py
@@ -18,7 +18,4 @@ class updateDateAppen(object):
 		current_date_format = current_date.strftime('%Y/%m/%d %H:%M:%S')
 		manageParameters.set_param_object("date_appen",current_date_format)
 		os.environ["date_appen"] = current_date_format
-		print "----------------------------"
-		print datetime.strptime(current_date_format,'%Y/%m/%d %H:%M:%S')
-		print "----------------------------"
 
