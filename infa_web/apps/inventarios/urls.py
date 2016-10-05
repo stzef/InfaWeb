2dac36087e774b6cc118cea94b7dc7b22c1f71c8
carlosturnerbenites <carlosturnerbenites@gmail.com>
2016-10-05 07:44:33 -0500

    Modificaiones en consultas de Forms y añadiendo restricciones de logeo a URLs

--- infa_web/apps/inventarios/urls.py
+++ infa_web/apps/inventarios/urls.py
@@ -1,17 +1,19 @@
 from django.conf.urls import patterns, include, url
 from infa_web.apps.inventarios.views import *
+
+from django.contrib.auth.decorators import login_required
  
 urlpatterns = [
-	url(r'^inventory/$', InventoryView.as_view(), name = 'inventory'),
-	url(r'^inventory/list/$', InventoryListView.as_view(), name = 'inventory_list'),
-	url(r'^inventory/last/$', inventory_latest, name = 'inventory_latest'),
-	url(r'^inventory/edit/$', inventory_edit, name = 'inventory_edit'),
-	url(r'^inventory/edit/get-list/$', articles_list_invini, name = 'articles_list_invini'),
-	url(r'^inventory/rename/$', get_name_arlo, name = 'get_name_arlo'),
-	url(r'^inventory/save/$', inventory_save, name = 'inventory_save'),
-	url(r'^inventory/save-extra/$', inventory_save_extra, name = 'inventory_save_extra'),
-	url(r'^inventory/report/$', InventoryReport.as_view(), name = 'inventory_report'),
-	url(r'^inventory/pdf/$', InventoryPDF.as_view(), name = 'inventory_pdf'),
-	url(r'^inventory/stocks/report/$', InventoryReportStocks.as_view(), name = 'inventory_stocks_report'),
-	url(r'^inventory/stocks/pdf/$', InventoryPDFStocks.as_view(), name = 'inventory_stocks_pdf'),
+	url(r'^inventory/$', login_required(InventoryView.as_view()), name = 'inventory'),
+	url(r'^inventory/list/$', login_required(InventoryListView.as_view()), name = 'inventory_list'),
+	url(r'^inventory/last/$', login_required(inventory_latest), name = 'inventory_latest'),
+	url(r'^inventory/edit/$', login_required(inventory_edit), name = 'inventory_edit'),
+	url(r'^inventory/edit/get-list/$', login_required(articles_list_invini), name = 'articles_list_invini'),
+	url(r'^inventory/rename/$', login_required(get_name_arlo), name = 'get_name_arlo'),
+	url(r'^inventory/save/$', login_required(inventory_save), name = 'inventory_save'),
+	url(r'^inventory/save-extra/$', login_required(inventory_save_extra), name = 'inventory_save_extra'),
+	url(r'^inventory/report/$', login_required(InventoryReport.as_view()), name = 'inventory_report'),
+	url(r'^inventory/pdf/$', login_required(InventoryPDF.as_view()), name = 'inventory_pdf'),
+	url(r'^inventory/stocks/report/$', login_required(InventoryReportStocks.as_view()), name = 'inventory_stocks_report'),
+	url(r'^inventory/stocks/pdf/$', login_required(InventoryPDFStocks.as_view()), name = 'inventory_stocks_pdf'),
 ]
