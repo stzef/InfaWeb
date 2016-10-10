f183bc926414d600f6ccebc23da08b51cdcfd0c3
Juan <juancsandoval86@gmail.com>
2016-10-10 11:39:52 -0500

    Maquetación vistas moviles

--- infa_web/apps/m/urls.py
+++ infa_web/apps/m/urls.py
@@ -4,7 +4,12 @@ from infa_web.apps.m.views import *
 
 url = [
 	url(r'^m/dashboard$', mDashboard, name='m_dashboard'),
-
+	url(r'^m/choose-client$', mFacChooseClient, name='m_choose_client'),
+	url(r'^m/search-client$', mFacSearchClient, name='m_search_client'),
+	url(r'^m/order$', mFacOrder, name='m_order'),
+	url(r'^m/order2$', mFacOrder2, name='m_order2'),
+	url(r'^m/choose-article$', mFacChooseArtice, name='m_choose_article'),
+	url(r'^m/pay$', mFacPay, name='m_pay'),
 ]
 
 urlpatterns = url
