48ae4983ee47552e13515de7d8f31db33c9056a3
Juan <juancsandoval86@gmail.com>
2016-10-14 11:52:51 -0500

    movil, busqueda de clientes vista de seleccion de clientes

--- infa_web/apps/m/urls.py
+++ infa_web/apps/m/urls.py
@@ -3,6 +3,8 @@ from django.contrib.auth.decorators import login_required
 from infa_web.apps.m.views import *
 
 url = [
+
+	# Pages
 	url(r'^m/dashboard$', mDashboard, name='m_dashboard'),
 	url(r'^m/choose-client$', mFacChooseClient, name='m_choose_client'),
 	url(r'^m/search-client$', mFacSearchClient, name='m_search_client'),
@@ -10,6 +12,11 @@ url = [
 	url(r'^m/order2$', mFacOrder2, name='m_order2'),
 	url(r'^m/choose-article$', mFacChooseArtice, name='m_choose_article'),
 	url(r'^m/pay$', mFacPay, name='m_pay'),
+	url(r'^m/third-party/add$', mThirdPartyAdd, name='m_third_party_add'),
+
+	# Ajax
+	url(r'^m/third-party/list$', mThirtyPartyList, name='m_ajax_third_party_list')
+
 ]
 
 urlpatterns = url
--- infa_web/apps/m/views.py
+++ infa_web/apps/m/views.py
@@ -1,13 +1,38 @@
 from django.shortcuts import render
+from django.core import serializers
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.db.models import Q
+
+
+from infa_web.apps.terceros.models import Tercero
 
 def mDashboard(request):
 	return render(request, 'm/m_dashboard.html')
 
 def mFacChooseClient(request):
-	return render(request, 'm/m_fac_choose_client.html')
+	# Consultar cliente mostrador
+	clienteMostrador = 1
+
+	# Agregar contexto
+	context = {
+		'clienteMostrador' : clienteMostrador
+	}
+
+	return render(request, 'm/m_fac_choose_client.html', context)
 
 def mFacSearchClient(request):
-	return render(request, 'm/m_fac_search_client.html')
+	# Consultar Terceros
+	# Priorizar Consulta
+	mostrador = 1
+	terceros = Tercero.objects.using(request.db).exclude(idterce=mostrador)[:10]
+
+	# Agregar a contexto
+	context = {
+		'terceros' : terceros
+	}
+
+	return render(request, 'm/m_fac_search_client.html', context)
 
 def mFacOrder(request):
 	return render(request, 'm/m_fac_order.html')
@@ -20,3 +45,30 @@ def mFacChooseArtice(request):
 
 def mFacPay(request):
 	return render(request, 'm/m_fac_pay.html')
+
+def mThirdPartyAdd(request):
+	return render(request, 'm/m_third_party_add.html')
+
+
+## Request Ajax basadas en JSON
+@csrf_exempt
+def mThirtyPartyList(request):
+
+	#filtro razonsocial | identificacion
+	query = request.GET.get('q', None)
+
+	# obtener terceros
+	clienteMostrador = 1
+	clientes = Tercero.objects.using(request.db).exclude(idterce=clienteMostrador)
+
+	# filtrar clientes
+	if query is not None:
+		clientes = clientes.filter(
+			Q(rasocial__icontains=query) | Q(idterce__contains=query)
+		)
+
+	# serializar respuesta
+	data = serializers.serialize("json", clientes, fields=('idterce', 'rasocial'))
+
+	return JsonResponse(data, safe=False)
+	# responder
