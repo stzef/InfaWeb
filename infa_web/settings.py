daa513489c71fc4867b08824c3a41d68d33aa905
Carlos Alonso Turner Benites <carlosturnerbenites@gmail.com>
2017-03-27 16:15:06 -0500

    Areglo de estaticos

--- infa_web/settings.py
+++ infa_web/settings.py
@@ -198,7 +198,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_ROOT = 'static'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
