22c4b56524ce4dc7486cc69855866ad8b3f35e74
Carlos Alonso Turner Benites <carlosturnerbenites@gmail.com>
2017-04-18 11:46:40 -0500

    Ajustes de tirilla de impresion

--- infa_web/apps/POS/urls.py
+++ infa_web/apps/POS/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 #url(r'^articles/$', login_required(ArticleList.as_view()), name = 'list-articles'),
 url = [
 	url(r'^pos/$', login_required(BillCreate.as_view()), name = 'pos'),
-	url(r'^some_view/$', login_required(some_view), name = 'some_view'),
+	url(r'^pos/print$', login_required(some_view), name = 'some_view'),
 ]
 
 urlpatterns = url
--- infa_web/apps/POS/views.py
+++ infa_web/apps/POS/views.py
@@ -77,12 +77,29 @@ from django.http import HttpResponse
 from reportlab.lib import colors
 from reportlab.lib.pagesizes import A4, inch, landscape, portrait
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph
-from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.enums import TA_CENTER
+
 
 def some_view(request):
 	# Create the HttpResponse object with the appropriate PDF headers.
 	response = HttpResponse(content_type='application/pdf')
 	response['Content-Disposition'] = 'attachment; filename="somefilename.pdf"'
+
+	manageParameters = ManageParameters(request.db)
+	data = request.GET
+
+	# Datos de Prueba
+	"""usuario = Usuario.objects.using(request.db).filter()[0]
+
+	talonario_MOS = usuario.ctalomos
+	talonario_POS = usuario.ctalopos"""
+	# Datos de Prueba
+
+	cfac = data.get('cfac')
+
+	factura = Fac.objects.using(request.db).get(cfac=cfac)
+	factura_deta = Facdeta.objects.using(request.db).filter(cfac=factura)
 
 	# Create the PDF object, using the response object as its "file."
 	#p = canvas.Canvas(response)
@@ -93,20 +110,40 @@ def some_view(request):
 	#p.drawString(100, 100, "Hello world.")
 	#p.setPageSize((200, 700)) #some page size, given as a tuple in points
 
-	doc = SimpleDocTemplate(response, pagesize=A4, rightMargin=5,leftMargin=5, topMargin=5,bottomMargin=5)
-	doc.pagesize = portrait((200, 700))
+	doc = SimpleDocTemplate(response, pagesize=A4, rightMargin=10,leftMargin=10, topMargin=0,bottomMargin=40)
+	doc.pagesize = portrait((190, 1900))
+
+	hr_linea = "----------------------------------------------------------"
+	hr_puntos = "........................................................."
 
 	elements = []
 
-	data = [
-		["Descripcion", "Cant", "Vr. Tot"],
-		["MANTENIMIENTO Y BACKUP", "1", "60.000"],
-		["MANTENIMIENTO PORTATILES", "2", "100.000"],
-		["MANTENIMIENTO PORTATIL CO", "1", "50.000"],
+	data_header = [
+		["HELADERIA  Y CAFETERIA LA GUACA No 3"],
+		["JEANNETTE PALENCIA DIAZ"],
+		["Iden. 39.563.805-1"],
+		["Regimen Comun"],
+		["Cra 10 25-52 TEL 8954573 Girardot"],
+		#["MANTENIMIENTO Y BACKUP", "1", "60.000"],
+		#["MANTENIMIENTO PORTATILES", "2", "100.000"],
+		#["MANTENIMIENTO PORTATIL CO", "1", "50.000"],
 	]
 
-	style = TableStyle([
+	data = [
+		["Descripcion", "Cant", "Vr. Tot"],
+		#["MANTENIMIENTO Y BACKUP", "1", "60.000"],
+		#["MANTENIMIENTO PORTATILES", "2", "100.000"],
+		#["MANTENIMIENTO PORTATIL CO", "1", "50.000"],
+	]
+
+	for facdeta in factura_deta:
+		data.append([facdeta.carlos.ncorto[:15],str(facdeta.canti),str(facdeta.vtotal)])
+
+	data.append(["Total","-->",str(factura.vttotal)])
+
+	style_table_header = TableStyle([
 		('ALIGN',(1,1),(-2,-2),'RIGHT'),
+		#('FONTSIZE', (0, 0), (2, 2), 5),
 		('TEXTCOLOR',(1,1),(-2,-2),colors.red),
 		('VALIGN',(0,0),(0,-1),'TOP'),
 		('TEXTCOLOR',(0,0),(0,-1),colors.blue),
@@ -117,17 +154,49 @@ def some_view(request):
 		('BOX', (0,0), (-1,-1), 0.25, colors.black),
 	])
 
+	style_table_facdeta = TableStyle([
+		('ALIGN',(1,1),(-2,-2),'RIGHT'),
+		#('FONTSIZE', (0, 0), (2, 2), 5),
+		('TEXTCOLOR',(1,1),(-2,-2),colors.red),
+		('VALIGN',(0,0),(0,-1),'TOP'),
+		('TEXTCOLOR',(0,0),(0,-1),colors.blue),
+		('ALIGN',(0,-1),(-1,-1),'CENTER'),
+		('VALIGN',(0,-1),(-1,-1),'MIDDLE'),
+		('TEXTCOLOR',(0,-1),(-1,-1),colors.green),
+		#('INNERGRID', (0,0), (-1,-1), 0.25, colors.black),
+		#('BOX', (0,0), (-1,-1), 0.25, colors.black),
+	])
+
 	#Configure style and word wrap
 	s = getSampleStyleSheet()
-	s = s["BodyText"]
+
+	s.add(ParagraphStyle(name='tirilla',fontSize=8,leading=12))
+	s.add(ParagraphStyle(name='header',fontSize=8,leading=12,alignment=TA_CENTER))
+
+	bodytext = s["tirilla"]
+	headertext = s["header"]
 	#s.wordWrap = 'CJK'
-	s.wordWrap = 'LTR'
-	data2 = [[Paragraph(cell, s) for cell in row] for row in data]
+	bodytext.wordWrap = 'LTR'
+	data2 = [[Paragraph(cell, bodytext) for cell in row] for row in data]
 	t=Table(data2)
-	t.setStyle(style)
+	t.setStyle(style_table_facdeta)
+
+	data2_header = [[Paragraph(cell, headertext) for cell in row] for row in data_header]
+	t_header=Table(data2_header)
+	t_header.setStyle(style_table_header)
 
 	#Send the data and build the file
+	elements.append(t_header)
+	elements.append(Paragraph("<br/>Factura No. %s \t\t" % factura.cfac,s['tirilla']))
+	elements.append(Paragraph("Fecha : %s \t\t" % factura.femi,s['tirilla']))
+	#elements.append(Paragraph("Cliente : %s\t\t" % factura.citerce.nomcomer,s['tirilla']))
+	#elements.append(Paragraph("Caja : %s\t\t" % factura.ccaja.ncaja,s['tirilla']))
+	elements.append(Paragraph("Atendido por : %s \t<br/>" % factura.cvende.nvende,s['tirilla']))
 	elements.append(t)
+	elements.append(Paragraph("GRACIAS POR SU VISITA LO ESPERAMOS PUEDES CONTAR CON NOSOTROS" ,s['tirilla']))
+	elements.append(Paragraph(hr_linea ,s['tirilla']))
+	elements.append(Paragraph("AppEm - Software para administracion de Empresas sitematizaref@gmail.com" ,s['tirilla']))
+	elements.append(Paragraph(hr_linea ,s['tirilla']))
 	doc.build(elements)
 
 
