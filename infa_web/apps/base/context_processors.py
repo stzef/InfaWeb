af1e7d97c32d0fc9373d0ebf4c45eef978f27938
Carlos Alonso Turner Benites <carlosturnerbenites@gmail.com>
2017-05-09 15:20:08 -0500

    commit de apoyo

--- infa_web/apps/base/context_processors.py
+++ infa_web/apps/base/context_processors.py
@@ -2,10 +2,13 @@ from infa_web.apps.usuarios.models import Usuario
 
 def var_globals(request):
 	sucursal = None
+	subdomain = None
 	if request.user.is_authenticated():
 		user_appem = Usuario.objects.using(request.db).get(user=request.user)
 		sucursal = user_appem.csucur.nsucur
+	if 'subdomain' in request:
+		subdomain = request.subdomain
 	return {
 		'sucursal':sucursal,
-		'subdomain':request.subdomain,
+		'subdomain':subdomain,
 	}
