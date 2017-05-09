a9d7d04d98d3001a162c60610f24497c01120588
Carlos Alonso Turner Benites <carlosturnerbenites@gmail.com>
2017-05-09 15:17:02 -0500

    Ajustes personales

--- infa_web/apps/base/context_processors.py
+++ infa_web/apps/base/context_processors.py
@@ -1,8 +1,10 @@
 from infa_web.apps.usuarios.models import Usuario
 
 def var_globals(request):
-	user_appem = Usuario.objects.using(request.db).get(user=request.user)
-	sucursal = user_appem.csucur.nsucur
+	sucursal = None
+	if request.user.is_authenticated():
+		user_appem = Usuario.objects.using(request.db).get(user=request.user)
+		sucursal = user_appem.csucur.nsucur
 	return {
 		'sucursal':sucursal,
 		'subdomain':request.subdomain,
