def2353730411403f994f02092637aa36fb13ca2
Cristhian Morales <sistematizaref.programador4@gmail.com>
2017-07-28 11:31:04 -0500

    Gestor de impresoras

--- infa_web/printers.py
+++ infa_web/printers.py
@@ -1,20 +1,10 @@
 import subprocess
+def send_to_print(file_path, name_printer):
+	print("file_path")
+	print(file_path)
+	print('name_printer')
+	print(name_printer)
+	cmd = "/usr/bin/lpr -P %s %s" % (name_printer, file_path)
 
-class ManagePrinter(object):
-	"""docstring for ManagePrinter"""
-	def __init__(self):
-		super(ManagePrinter, self).__init__()
-		self.name_printer = "default_printer"
-
-	def send_to_print(self, file_path, name_printer):
-		
-		name_printer = name_printer if name_printer is not None else self.name_printer
-
-		print(name_printer)
-
-		cmd = "/usr/bin/lpr %s" % (file_path)
-		if self.name_printer is not None:
-			cmd = "/usr/bin/lpr -P %s %s" % (name_printer, file_path)
-
-		lpr =  subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-		return lpr
+	lpr =  subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	return lpr
