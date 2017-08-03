ee760647d5936956f7a56ab6969686461125a27c
Carlos Alonso Turner Benites <carlosturnerbenites@gmail.com>
2017-08-03 08:11:00 -0500

    Commit de apoyo

--- infa_web/printers.py
+++ infa_web/printers.py
@@ -1,7 +1,7 @@
 import subprocess
 def send_to_print(file_path, name_printer):
 	cmd_print = '/usr/bin/lpr -P %s %s' % (name_printer, file_path)
-	# subprocess.Popen(cmd_print, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	subprocess.Popen(cmd_print, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
 	cmd_delete = 'rm %s' % file_path
 	# subprocess.Popen(cmd_delete, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
